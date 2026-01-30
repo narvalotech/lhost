7da5e8a9334819369c39654c1daf8360e74da33f
Jonathan Rico <jonathan@rico.live>
2026-01-29 19:51:33 -0700

    fix up MITM script
    it werks!

--- host.lisp
+++ host.lisp
@@ -2989,21 +2989,21 @@
    (arm-mitm)
 
    (init-controller hci)
-
    (hci-set-scan-param hci)
-   (hci-set-scan-enable hci t)
 
    (let ((conn-peripheral)
          (conn-central)
          ;; Shadow active-conns
          (*active-conns* '())
-         (address (wait-for-scan-report hci (lambda (x) (name? "Hello!" x)))))
+         )
 
      (log-inf "#######################################")
      (log-inf (format nil "Wait for connection to peripheral"))
-     (hci-set-scan-enable hci nil)
-     (hci-create-connection hci (copy-tree address))
 
+     (hci-set-scan-enable hci t)
+     (let ((address (wait-for-scan-report hci (lambda (x) (name? "bob" x)))))
+       (hci-set-scan-enable hci nil)
+       (hci-create-connection hci (copy-tree address)))
      (let ((conn-evt (wait-for-conn hci)))
        (setf conn-peripheral (getf conn-evt :handle))
        (setf (getf (getf *active-conns* conn-peripheral) :our-address)
@@ -3012,13 +3012,22 @@
              (getf conn-evt :address)))
 
      (setf (get-smp-context conn-peripheral) '())
+     ;; Pair peripheral
+     (log-inf "#######################################")
+     (log-inf "Pairing peripheral")
+     (start-security hci conn-peripheral)
+     ;; (smp-send-pairing-req hci conn-peripheral)
+
+     (wait-for-encryption hci conn-peripheral)
 
      (log-inf "#######################################")
      (log-inf (format nil "Wait for connection to central"))
 
      (start-advertising hci (list
                              (make-ad :flags '(#x01)) ; LE General discoverable
-                             (make-ad-name "lhost")))
+                             (make-ad :class-uuid-16-incomplete
+                                      (make-c-int :u16 +gatt-uuid-heart-rate-service+))
+                             (make-ad-name "alice")))
 
      (let ((conn-evt (wait-for-conn hci)))
        (setf conn-central (getf conn-evt :handle))
@@ -3029,23 +3038,19 @@
 
      (setf (get-smp-context conn-central) '())
 
-     ;; Pair peripheral
-     (log-inf "#######################################")
-     (log-inf "Pairing peripheral")
-     (smp-send-pairing-req hci conn-peripheral)
-     (wait-for-encryption hci conn-peripheral)
-
      ;; Pair central
-     (log-inf "#######################################")
-     (log-inf "Pairing central")
-     (smp-send-security-req hci conn-central)
-     (wait-for-encryption hci conn-central)
+     (let ((*bonds* (make-hash-table)))
+       (log-inf "#######################################")
+       (log-inf "Pairing central")
+       (smp-send-security-req hci conn-central)
+       (wait-for-encryption hci conn-central))
 
      ;; Process backed-up ATT packets
      (log-inf "#######################################")
      (log-inf "Drain queue")
      (start-mitm conn-central conn-peripheral)
      (drain-mitm-att-queue hci)
+     (drain-mitm-signalling-queue hci)
 
      ;; At this point, ATT packets are transmitted to the other
      ;; connection.
@@ -3055,7 +3060,7 @@
      (log-inf "#######################################")
 
      ;; Sleep for a while, but still process packets
-     (loop for i from 0 to 100 do
+     (loop for i from 0 to 1000 do
        (progn
          (drain-rxq hci)
          (do-idle-work hci)
@@ -3075,8 +3080,6 @@
 ;; TODO:
 ;; - REPL usage
 ;; - clone GATT table
-;; - central pairing
-;; - MITM for JW
 ;;
 ;; Okay so here's what we really need:
 ;;
